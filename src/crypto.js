36993b1fc474ffc24fd35c6021c1f15bc1821fc9
AnandXPraveen <59990137+AnandXPraveen@users.noreply.github.com>
2026-01-01 06:59:53 +0530

    Create crypto.js

---
+++ src/crypto.js
@@ -0,0 +1,9 @@
+// Web Crypto API wrapper
+// AES-GCM encryption for data
+
+class CryptoEngine {
+  async encrypt(data, key) { }
+  async decrypt(data, key) { }
+}
+
+const crypto = new CryptoEngine();
